4f5cd4eb06e5c817a11db284bb4291e437e8ae2a
muhamaddanil <muhammaddanil790@gmail.com>
2023-03-05 14:18:37 +0800

    ok

--- application/views/template_admin.php
+++ application/views/template_admin.php
@@ -4,11 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sistem Monitoring</title>
+  <title>Sistem Penjadwalan</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="<?php echo base_url('assets/img/long-logo.png') ?>">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url() ?>assets/admin_lte2/plugins/fontawesome-free/css/all.min.css">
+  <link rel=" stylesheet" href="<?= base_url() ?>assets/admin_lte2/plugins/fontawesome-free/css/all.min.css">
 
   <link rel="stylesheet" href="<?= base_url() ?>assets/admin_lte2/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
 
