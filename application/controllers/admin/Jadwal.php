c2c3c295fe53f57d40dc8f8b3bda7a3d191edd74
muhamaddanil <muhammaddanil790@gmail.com>
2023-03-07 20:29:09 +0800

    oke

--- application/controllers/admin/Jadwal.php
+++ application/controllers/admin/Jadwal.php
@@ -125,8 +125,6 @@ class Jadwal extends MY_Controller
         $arrayKelas = $this->M_kelas->get_all_kelas($this->uri->segment(4))->result_array();
         $constValue = 0.45;
 
-        $labelDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
-
         $key = 0;
         foreach ($arrayKelas as $idKelas) {
             $arrayMapel[$idKelas['kelas_alias']] = $this->M_pelajaran->count_mapel_kelas($idKelas['id'])->result_array();
@@ -136,7 +134,6 @@ class Jadwal extends MY_Controller
             if (!empty($tempArray)) {
 
                 foreach ($tempArray as $index => $value) {
-
                     $tempArray[$index]['cg_value'] = $value['total_kelas'] / $value['mapel_jp'];
                     $tempArray[$index]['conflict_value'] = round($constValue / $tempArray[$index]['cg_value'], 4);
                 }
@@ -147,9 +144,6 @@ class Jadwal extends MY_Controller
                     array_multisort(array_column($tempArray, 'mapel_jp'), SORT_ASC, $tempArray);
                 else
                     array_multisort(array_column($tempArray, 'guru_nama'), SORT_DESC, $tempArray);
-                // $this->debug($tempArray);
-                // $this->debug(array_count_values(array_column($tempArray, 'mapel_jp')));
-                // die;
 
                 $mx = 0;
                 $j = 0;
@@ -194,7 +188,6 @@ class Jadwal extends MY_Controller
                     $mx++;
                 }
 
-
                 $key++;
             }
             // $this->debug($arrayHari);
@@ -202,7 +195,7 @@ class Jadwal extends MY_Controller
         }
         // $this->debug($arrayHari);
         // die;
-
+        $labelDay = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
         foreach ($arrayHari as $ary) {
             foreach ($ary as $idx => $ar) {
                 foreach ($ar as $r) {
@@ -223,64 +216,5 @@ class Jadwal extends MY_Controller
 
         $this->session->set_flashdata('msg', 'Berhasil Mengenerate Jadwal Kelas ' . $this->uri->segment(4) . ' Dengan Estimasi Waktu ' . $waktu_eksekusi . 'ms|success');
         redirect('admin/jadwal');
-    }
-
-    public function get_jp_guru($groupedDayArray)
-    {
-        foreach ($groupedDayArray as $key_hari => $value_hari) {
-            foreach ($value_hari as $key_perhari => $value_perhari) {
-                $jp_guru = 0;
-                $jp_kelas = 0;
-                foreach ($value_hari as $key => $value) {
-                    if ($value['guru_kode'] == $value_perhari['guru_kode']) {
-                        $jp_guru = $jp_guru + $value['mapel_jp'];
-                    }
-                    if ($value['id_kelas'] == $value_perhari['id_kelas']) {
-                        $jp_kelas = $jp_kelas + $value['mapel_jp'];
-                    }
-                }
-                $groupedDayArray[$key_hari][$key_perhari]['total_jp_guru'] = $jp_guru;
-                $groupedDayArray[$key_hari][$key_perhari]['total_jp_kelas'] = $jp_kelas;
-            }
-        }
-        return $groupedDayArray;
-    }
-
-    public function get_jp_guru_copy($data, $guru_kode)
-    {
-        $jp = 0;
-        foreach ($data as $key => $value) {
-            if ($data['guru_kode'] == $guru_kode) {
-                $jp = $jp + $data['mapel_jp'];
-            }
-        }
-        return $jp;
-    }
-
-    public function sort_by_guru($groupedDayArray)
-    {
-        foreach ($groupedDayArray as $key2 => $today) {
-            $sortArray = array();
-            foreach ($today as $class) {
-                foreach ($class as $key => $value) {
-                    if (!isset($sortArray[$key])) {
-                        $sortArray[$key] = array();
-                    }
-                    $sortArray[$key][] = $value;
-                }
-            }
-            array_multisort($sortArray['guru_kode'], SORT_ASC, $groupedDayArray[$key2]);
-        }
-
-        return $groupedDayArray;
-    }
-
-    function convert_1dimension_array($array, $column)
-    {
-        $arrayReturn = array();
-        foreach ($array as $ar) {
-            $arrayReturn[] = $ar[$column];
-        }
-        return $arrayReturn;
     }
 }
